0dabbef61ec256c8fd2b2bf33f6a7b9252112ad6
A-sato0422 <achira0422@gmail.com>
2026-02-12 19:54:01 +0900

    ・データ詳細画面を新規作成しました ・詳細画面のルートを追加しました。

---
+++ src/MovieDetail.tsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router";
+
+function MovieDetail() {
+    const {movieId} = useParams();
+    return (
+        <div>{movieId} 映画詳細ページ</div>
+    );
+}
+
+export default MovieDetail;
